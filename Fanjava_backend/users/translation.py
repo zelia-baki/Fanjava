daf06ad1c3b03cb752e11d4a6947ac25446399d4
root <root@vmi2970672.contaboserver.net>
2025-12-28 13:42:41 +0100

    avy amn serveru

--- Fanjava_backend/users/translation.py
+++
@@ -1,8 +0,0 @@
-# users/translation.py
-
-from modeltranslation.translator import register, TranslationOptions
-from .models import Entreprise
-
-@register(Entreprise)
-class EntrepriseTranslationOptions(TranslationOptions):
-    fields = ('description',)
